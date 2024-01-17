1abf2ad5e88a405c3a68e4326800c59992d71694
Vatthana Boulom <boulom.vatthana.pro@gmail.com>
2024-01-17 21:38:27 +1300

    Add device_preview and shared_preferences_foundation dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
 // Importing necessary packages
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_app/quiz.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 // The main function is the entry point of the application.
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   // runApp is a Flutter function that inflates the given widget and attaches it to the screen.
   // Quiz is the root widget of the application.
-  runApp(const Quiz());
+  runApp(DevicePreview(
+      builder: (context) => const Quiz(), enabled: !kReleaseMode));
 }
