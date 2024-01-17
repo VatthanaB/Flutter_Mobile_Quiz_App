4109934484a275ae36d2b4bd0cc09e3b3ab6aaa1
Vatthana Boulom <boulom.vatthana.pro@gmail.com>
2024-01-17 20:39:02 +1300

    Add Firebase Core dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 // Importing necessary packages
 import 'package:flutter/material.dart';
 import 'package:quiz_app/quiz.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 // The main function is the entry point of the application.
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // runApp is a Flutter function that inflates the given widget and attaches it to the screen.
   // Quiz is the root widget of the application.
   runApp(const Quiz());
